9f5220af4e5f752de954ddc2f046489872fce03b
Fereshteh Heidari <fereshte121@gmail.com>
2021-02-03 14:12:20 -0800

    Delete items from Custom TODO list

--- app.js
+++ app.js
@@ -2,76 +2,161 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose=require("mongoose")
-
+const mongoose = require("mongoose")
+const names = []
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
-const itemsSchema={
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const itemsSchema = {
   name: String
 };
-const Item= mongoose.model("Item",itemsSchema);
+const Item = mongoose.model("Item", itemsSchema);
 
-const item1= new Item({name:"Welcome to your todoList!"});
-const item2= new Item({name:"Hit the + button to add a new item."});
-const item3=new Item({name:"<-- Hit this to delete an item."});
-const defaultItems=[item1,item2,item3];
+const item1 = new Item({
+  name: "Welcome to your todoList!"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+const defaultItems = [item1, item2, item3];
 
+const listSchema = {
 
+  name: String,
 
+  items: [itemsSchema]
+
+}
+
+const List = mongoose.model("List", listSchema)
 
 
 app.get("/", function(req, res) {
 
-    Item.find(function(err,foundItems){
+  Item.find(function(err, foundItems) {
 
-      if(foundItems.length===0){
-      Item.insertMany(defaultItems,function(err){});
-        res.redirect("/");
-      } else{
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {});
+      res.redirect("/");
+    } else {
 
 
-        res.render("list", {listTitle: "Today", newListItems: foundItems});
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItems
+      });
     }
 
-})
+  })
 })
 
-app.post("/", function(req, res){
+
+
+app.get("/:customListName", function(req, res) {
+  const customListName = req.params.customListName;
+
+
+
+  List.findOne({
+    name: customListName
+  }, function(err, foundList) {
+
+    if (!foundList) {
+      //create a new list
+      const list = new List(
+
+        {
+          name: customListName,
+          items: defaultItems
+        })
+      list.save();
+      res.redirect("/" + customListName);
+
+    } else {
+      //Show an existing list
+      res.render("list", {
+        listTitle: foundList.name,
+        newListItems: foundList.items
+      })
+
+    }
+  });
+
+
+});
+
+
+
+app.post("/", function(req, res) {
 
   const itemName = req.body.newItem;
+  const title = req.body.list;
+
   const newItem = new Item({
-     name: itemName
-   });
-newItem.save();
-res.redirect("/");
+    name: itemName
+  });
+
+  if (title === "Today") {
+
+    newItem.save();
+    res.redirect("/");
+  } else {
+    List.findOne({name:title},function(err,result){
+
+result.items.push(newItem);
+result.save();
+res.redirect("/"+title);
+
+    })
+
+
+  }
+
+
 
 
 });
 
-app.post("/delete",function(req,res){
-console.log(req.body.checkbox);
+app.post("/delete", function(req, res) {
 
-Item.deleteOne({_id:req.body.checkbox},function(err){});
+const Title= req.body.listName;
+if(Title==="Today"){
+  Item.deleteOne({
+    _id: req.body.checkbox
+  }, function(err) {});
 
-res.redirect("/");
+  res.redirect("/");
+} else {
+
+List.findOneAndUpdate({name:req.body.listName},{$pull:{items:{_id:req.body.checkbox}}},function(err,result){
+
+
+})
+res.redirect("/"+ req.body.listName)
+
+}
+
+
 })
 
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
-});
+})
